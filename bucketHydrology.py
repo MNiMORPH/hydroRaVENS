34176261fb83fa9b69515c94413259fd9a2ee47f
Andy Wickert <andy@northernwidget.com>
2019-10-24 15:13:59 -0500

    Separate driver and module

--- bucketHydrology.py
+++ bucketHydrology.py
@@ -118,23 +118,3 @@ class buckets(object):
         plt.xlabel('Time [days]', fontsize=14)
         plt.show()
 
-
-# Program below the class
-import numpy as np
-from numpy.random import poisson
-from matplotlib import pyplot as plt
-#plt.ion()
-
-rain = poisson(.2, 100) # Convert to an import for real data
-dt = 1. # day
-        
-# Change these parameters with an input file or script, eventually
-# Arbitrary units; will be made real in an actual landscape
-res_surface = reservoir(t_efold=1., f_to_discharge=0.5, Hmax=10.)
-res_deep = reservoir(t_efold=10., f_to_discharge=1., Hmax=np.inf)
-
-strat_column = [res_surface, res_deep]
-
-watershed = buckets(reservoir_list=strat_column, dt=dt)
-watershed.run(rain)
-watershed.plot()
