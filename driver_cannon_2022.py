fccf31209241dd6181f14ae8b3990ab7e2244b8a
Andy Wickert <andy@northernwidget.com>
2022-10-31 00:17:08 -0500

    Placeholder to set number of spin-up rounds using input data

--- driver_cannon_2022.py
+++ driver_cannon_2022.py
@@ -11,6 +11,8 @@ self = b
 self.compute_water_year()
 self.compute_ET_multiplier()
 self.compute_ET()
+#self.run() #Spin-up
+#self._timestep_i = 0. # Restart
 self.run()
 self.computeNSE(verbose=True)
 self.plot()
