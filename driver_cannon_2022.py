230705c22e3fee4e5cc0ab11bda1d579b4623541
Andy Wickert <andy@northernwidget.com>
2022-10-31 04:03:02 -0500

    Restore snow (mass balance keeps working!) and plotting

--- driver_cannon_2022.py
+++ driver_cannon_2022.py
@@ -8,7 +8,7 @@ b.initialize('cannon_cfg_tmp.yml')
 
 # To lump into internal functions later but test here for now
 self = b
-self.hydrodata['Mean Temperature [C]'] += 40 # ensure no snowmelt processes
+#self.hydrodata['Mean Temperature [C]'] += 40 # ensure no snowmelt processes
 self.compute_water_year()
 self.compute_ET_multiplier()
 self.compute_ET()
@@ -16,7 +16,7 @@ self.compute_ET()
 #self._timestep_i = 0. # Restart
 self.run()
 self.computeNSE(verbose=True)
-#self.plot()
+self.plot()
 
 import numpy as np
 for time_step in self.hydrodata.index:
