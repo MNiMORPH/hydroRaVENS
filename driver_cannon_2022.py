5b9fa9d163b8cefa3e92368ff3e0486c0ea91a33
Andy Wickert <andy@northernwidget.com>
2022-10-31 11:50:15 -0500

    Current testing + NSE

--- driver_cannon_2022.py
+++ driver_cannon_2022.py
@@ -4,7 +4,7 @@
 
 import bucketHydrology
 b = bucketHydrology.Buckets()
-b.initialize('cannon_cfg_tmp.yml')
+b.initialize('cannon_cfg.yml')
 
 # To lump into internal functions later but test here for now
 self = b
@@ -12,14 +12,16 @@ self = b
 self.compute_water_year()
 self.compute_ET_multiplier()
 self.compute_ET()
-#self.run() #Spin-up
-#self._timestep_i = 0. # Restart
+self.run() #Spin-up
+self._timestep_i = 0. # Restart
+self.run() #Spin-up
+self._timestep_i = 0. # Restart
 self.run()
 self.computeNSE(verbose=True)
 self.plot()
 
-import numpy as np
-for time_step in self.hydrodata.index:
-    excess_mass_in_model = self.check_mass_balance(time_step)
-    subsurface_storage = self.hydrodata['Subsurface storage (modeled total) [mm]'][time_step]
-    print( '%0.3f' %excess_mass_in_model, '%0.3f' %subsurface_storage )
+#import numpy as np
+#for time_step in self.hydrodata.index:
+#    excess_mass_in_model = self.check_mass_balance(time_step)
+#    subsurface_storage = self.hydrodata['Subsurface storage (modeled total) [mm]'][time_step]
+#    print( '%0.3f' %excess_mass_in_model, '%0.3f' %subsurface_storage )
