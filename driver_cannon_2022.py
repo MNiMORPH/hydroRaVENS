c4dbc19b1d9c69889ad4fe09d84f3e6cc3107e1f
awickert <andy@northernwidget.com>
2023-06-25 00:11:57 -0500

    Move spin-up step into a loop.

--- driver_cannon_2022.py
+++ driver_cannon_2022.py
@@ -12,10 +12,9 @@ self = b
 self.compute_water_year()
 self.compute_ET_multiplier()
 self.compute_ET()
-self.run() #Spin-up
-self._timestep_i = 0. # Restart
-self.run() #Spin-up
-self._timestep_i = 0. # Restart
+for i in range(2):
+    self.run() #Spin-up
+    self._timestep_i = 0. # Restart
 self.run()
 self.computeNSE(verbose=True)
 self.plot()
